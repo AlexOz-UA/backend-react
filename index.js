9ab1d5b0a1c0449ae1b0fc69c15337e64582c8ed
AlexOz-UA <alexclans2015@gmail.com.com>
2023-08-01 17:23:50 +0200

    Backend

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import mysql from "mysql";
 import cors from "cors";
 import jwt, { verify } from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+dotenv.config();
+
+//hello
 
 const app = express();
 const db = mysql.createConnection({
@@ -25,7 +29,7 @@ const verifyJWT = (req, res, next) => {
   if (!token) {
     res.send("I need a token.");
   } else {
-    jwt.verify(token, "jwtSecret", (err, decoded) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
       if (err) {
         res.send(`${err}`);
       } else {
@@ -38,7 +42,297 @@ const verifyJWT = (req, res, next) => {
 
 app.get("/posts", verifyJWT, (req, res) => {
   const queryAllPostsSelect = "SELECT * FROM post";
-  queryRun(queryAllPostsSelect, res);
+  db.query(queryAllPostsSelect, (err, results) => {
+    try{
+      const blogsWithPopularity = [];
+
+      results.forEach((blog, index) => {
+        getLikes(blog.id, (err, numLikes) => {
+          if (err) {
+            return res.status(500).json({ error: 'Error fetching likes' });
+          }
+      
+            const blogWithPopularity = {
+              id: blog.id,
+              name: blog.name,
+              body: blog.body,
+              creator: blog.creator,
+              num_likes: numLikes,
+            };
+      
+            blogsWithPopularity.push(blogWithPopularity);
+      
+            if (index === results.length - 1) {
+              res.status(200).json(blogsWithPopularity);
+            }
+        });
+      });
+    }catch(error){
+      console.error("You have an error: " + err)
+    }
+  })
+});
+
+function getLikes(blogId, callback) {
+  const queryGetLikes = 'SELECT COUNT(*) AS num_likes FROM post_likes WHERE post_id = ?';
+  db.query(queryGetLikes, [blogId], (err, results) => {
+    if (err) {
+      console.error('Error fetching likes:', err);
+      return callback(err, null);
+    }
+    callback(null, results[0].num_likes);
+  });
+}
+
+app.get('/pagination/popular-blogs/:page/:limit', (req, res) => {
+  const page = req.params.page;
+  const limit = req.params.limit;
+  const offset = (page - 1) * limit;
+
+  const queryPopularBlogsSelect = `
+    SELECT id, name, body, creator FROM post 
+    ORDER BY (SELECT COUNT(*) FROM post_likes WHERE post.id = post_likes.post_id) DESC LIMIT ? OFFSET ?;`;
+
+  const queryTotalPopularBlogs = `
+    SELECT COUNT(*) AS totalItems FROM post;`;
+
+  db.query(queryPopularBlogsSelect, [parseInt(limit), offset], (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    db.query(queryTotalPopularBlogs, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      const totalItems = data[0].totalItems;
+      const blogsWithPopularity = [];
+
+      results.forEach((blog) => {
+        const queryLikesCount = `
+          SELECT COUNT(*) AS num_likes
+          FROM post_likes
+          WHERE post_id = ?`;
+
+        db.query(queryLikesCount, [blog.id], (err, likeData) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+
+          const blogWithPopularity = {
+            id: blog.id,
+            name: blog.name,
+            body: blog.body,
+            creator: blog.creator,
+            num_likes: likeData[0].num_likes,
+          };
+          blogsWithPopularity.push(blogWithPopularity);
+
+          if (blogsWithPopularity.length === results.length) {
+            res.status(200).json({ sortedBlogs: blogsWithPopularity, totalItems });
+          }
+        });
+      });
+    });
+  });
+});
+
+
+app.get("/pagination", (req, res) => {
+  const page = req.query.page;
+  const limit = req.query.limit;
+  const offset = (page - 1) * limit;
+
+  const queryPaginationPostsSelect = "SELECT * FROM post LIMIT ? OFFSET ?";
+
+  db.query(queryPaginationPostsSelect, [parseInt(limit), offset], (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const totalItemsQuery = "SELECT COUNT(*) AS totalItems FROM post";
+    db.query(totalItemsQuery, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      const totalItems = data[0].totalItems;
+      const blogsWithPopularity = [];
+
+      results.forEach((blog, index) => {
+        getLikes(blog.id, (err, numLikes) => {
+          if (err) {
+            return res.status(500).json({ error: 'Error fetching likes' });
+          }
+      
+            const blogWithPopularity = {
+              id: blog.id,
+              name: blog.name,
+              body: blog.body,
+              creator: blog.creator,
+              num_likes: numLikes,
+            };
+      
+            blogsWithPopularity.push(blogWithPopularity);
+      
+            if (index === results.length - 1) {
+              res.status(200).json({sortedBlogs: blogsWithPopularity, totalItems});
+            }
+        });
+      });
+    });
+  });
+});
+
+app.post("/pagination/categories", (req, res) => {
+  const page = req.query.page;
+  const limit = req.query.limit;
+  const offset = (page - 1) * limit;
+
+  const categoryIds = req.body.data.id;
+
+  const queryCategoryFilterPaginationSelect = `
+    SELECT id, name, body, creator FROM post
+    WHERE id IN (SELECT post_id FROM categories_post WHERE categories_id IN (${categoryIds}))
+    ORDER BY (SELECT COUNT(*) FROM post_likes WHERE post.id = post_likes.post_id) DESC
+    LIMIT ? OFFSET ?;`;
+
+  const queryTotalCategoryBlogs = `
+    SELECT COUNT(DISTINCT post.id) AS totalItems FROM post
+    WHERE id IN (SELECT post_id FROM categories_post WHERE categories_id IN (${categoryIds}));`;
+
+  db.query(queryCategoryFilterPaginationSelect, [parseInt(limit), offset], (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    db.query(queryTotalCategoryBlogs, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      const totalItems = data[0].totalItems;
+      const blogsWithPopularity = [];
+
+      results.forEach((blog) => {
+        const queryLikesCount = `
+          SELECT COUNT(*) AS num_likes FROM post_likes WHERE post_id = ?`;
+
+        db.query(queryLikesCount, [blog.id], (err, likeData) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+
+          const blogWithPopularity = {
+            id: blog.id,
+            name: blog.name,
+            body: blog.body,
+            creator: blog.creator,
+            num_likes: likeData[0].num_likes,
+          };
+          blogsWithPopularity.push(blogWithPopularity);
+
+          if (blogsWithPopularity.length === results.length) {
+            res.status(200).json({ sortedBlogs: blogsWithPopularity, totalItems });
+          }
+        });
+      });
+    });
+  });
+});
+
+app.post("/pagination/search", (req, res) => {
+  const page = req.query.page;
+  const limit = req.query.limit;
+  const postIds = req.body.data.postIds.join(','); 
+  const offset = (page - 1) * limit;
+
+  const querySearchPaginationSelect = `
+    SELECT id, name, body, creator FROM post
+    WHERE id IN (${postIds})
+    ORDER BY (SELECT COUNT(*) FROM post_likes WHERE post.id = post_likes.post_id) DESC
+    LIMIT ? OFFSET ?;`;
+
+  const queryTotalSearchBlogs = `
+    SELECT COUNT(*) AS totalItems FROM post
+    WHERE id IN (${postIds});`;
+
+  db.query(querySearchPaginationSelect, [parseInt(limit), offset], (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    db.query(queryTotalSearchBlogs, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      const totalItems = data[0].totalItems;
+      const blogsWithPopularity = [];
+
+      results.forEach((blog) => {
+        const queryLikesCount = `
+          SELECT COUNT(*) AS num_likes FROM post_likes WHERE post_id = ?`;
+
+        db.query(queryLikesCount, [blog.id], (err, likeData) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+
+          const blogWithPopularity = {
+            id: blog.id,
+            name: blog.name,
+            body: blog.body,
+            creator: blog.creator,
+            num_likes: likeData[0].num_likes,
+          };
+          blogsWithPopularity.push(blogWithPopularity);
+
+          if (blogsWithPopularity.length === results.length) {
+            res.status(200).json({ sortedBlogs: blogsWithPopularity, totalItems });
+          }
+        });
+      });
+    });
+  });
+});
+
+
+app.get("/posts-liked/:user_id", verifyJWT, (req, res) => {
+  const queryLikedPostsSelect = `SELECT * FROM post_likes WHERE user_id = ${req.params.user_id}`;
+  db.query(queryLikedPostsSelect, (err, data) => {
+    if (err) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    if (!data) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    let values = "";
+    data.forEach((element) => {
+      values += `${element.post_id},`;
+    });
+    let newValues = values.slice(0, values.length - 1);
+    console.log(newValues);
+    const queryCategorySelect = `SELECT * FROM post WHERE id IN (${newValues})`;
+    db.query(queryCategorySelect, (err, categoryData) => {
+      if (err) console.log(err);
+      res.status(200).json(categoryData);
+    });
+  });
 });
 
 app.get("/post/:id", (req, res) => {
@@ -46,16 +340,175 @@ app.get("/post/:id", (req, res) => {
   queryRun(queryOnePostSelect, res);
 });
 
+app.post("/post/like/:id", (req, res) => {
+  const payload = req.body.data;
+  const queryPostLike = "INSERT INTO post_likes (user_id, post_id) VALUES (?)";
+  const values = [payload.user_id, req.params.id];
+  queryRunWithData(queryPostLike, values, res);
+});
+
+app.post("/post/unlike/:id", (req, res) => {
+  const payload = req.body.data;
+  const queryPostUnlike = `DELETE FROM post_likes WHERE user_id = (?) AND post_id = ${req.params.id}`;
+  const values = [payload.user_id];
+  queryRunWithData(queryPostUnlike, values, res);
+});
+
+app.get("/post/is-liked/:id/:user_id", (req, res) => {
+  const queryPostIsliked = `SELECT * FROM post_likes WHERE user_id = ${req.params.user_id} AND post_id = ${req.params.id}`;
+  db.query(queryPostIsliked, (err, data) => {
+    console.log(data);
+    if (err) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    if (data[0] && data[0].id) {
+      console.log(data);
+      res.status(200).json({ likeStatus: true });
+    } else {
+      res.status(200).json({ likeStatus: false });
+    }
+  });
+});
+
+app.get("/post/likes-count/:id", (req, res) => {
+  const queryPostIsliked = `SELECT COUNT (id) AS count FROM post_likes WHERE post_id = ${req.params.id};`;
+  db.query(queryPostIsliked, (err, data) => {
+    console.log(data);
+    if (err) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    if (!data) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    res.status(200).json(data);
+  });
+});
+
+app.get("/posts-saved/:user_id", verifyJWT, (req, res) => {
+  const querySavedPostsSelect = `SELECT * FROM post_saved WHERE user_id = ${req.params.user_id}`;
+  db.query(querySavedPostsSelect, (err, data) => {
+    if (err) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    if (!data) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    let values = "";
+    data.forEach((element) => {
+      values += `${element.post_id},`;
+    });
+    let newValues = values.slice(0, values.length - 1);
+    console.log(newValues);
+    const queryCategorySelect = `SELECT * FROM post WHERE id IN (${newValues})`;
+    db.query(queryCategorySelect, (err, categoryData) => {
+      if (err) console.log(err);
+      res.status(200).json(categoryData);
+    });
+  });
+});
+
+app.post("/post/save/:id", (req, res) => {
+  const payload = req.body.data;
+  const queryPostsave = "INSERT INTO post_saved (user_id, post_id) VALUES (?)";
+  const values = [payload.user_id, req.params.id];
+  queryRunWithData(queryPostsave, values, res);
+});
+
+app.post("/post/unsave/:id", (req, res) => {
+  const payload = req.body.data;
+  const queryPostUnsave = `DELETE FROM post_saved WHERE user_id = (?) AND post_id = ${req.params.id}`;
+  const values = [payload.user_id];
+  queryRunWithData(queryPostUnsave, values, res);
+});
+
+app.get("/post/is-saved/:id/:user_id", (req, res) => {
+  const queryPostIssaved = `SELECT * FROM post_saved WHERE user_id = ${req.params.user_id} AND post_id = ${req.params.id}`;
+  db.query(queryPostIssaved, (err, data) => {
+    console.log(data);
+    if (err) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    if (data[0] && data[0].id) {
+      console.log(data);
+      res.status(200).json({ savedStatus: true });
+    } else {
+      res.status(200).json({ savedStatus: false });
+    }
+  });
+});
+
+app.get("/post/saved-count/:id", (req, res) => {
+  const queryPostIsliked = `SELECT COUNT (id) AS count FROM post_saved WHERE post_id = ${req.params.id};`;
+  db.query(queryPostIsliked, (err, data) => {
+    console.log(data);
+    if (err) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    if (!data) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    res.status(200).json(data);
+  });
+});
+
 app.get("/categories", (req, res) => {
   const queryCategoriesSelect = `SELECT * FROM categories`;
   queryRun(queryCategoriesSelect, res);
+});
+
+app.post("/categories-filter", (req, res) => {
+  const queryCategoryFilterSelect = `SELECT * FROM categories_post WHERE categories_id IN (${req.body.data.id})`;
+  db.query(queryCategoryFilterSelect, (err, data) => {
+    if (err) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    if (!data) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    let values = "";
+    data.forEach((element) => {
+      values += `${element.post_id},`;
+    });
+    let newValues = values.slice(0, values.length - 1);
+    console.log(newValues);
+    const queryCategorySelect = `SELECT * FROM post WHERE id IN (${newValues})`;
+    db.query(queryCategorySelect, (err, categoryData) => {
+      if (err) console.log(err);
+      res.status(200).json(categoryData);
+    });
+  });
+});
+
+app.post("/search-bar", (req, res) => {
+  console.log(req.body.data.searchBar);
+  const searchBarBlogsQuery = `SELECT * FROM post WHERE name LIKE '${req.body.data.searchBar}%'`;
+  db.query(searchBarBlogsQuery, (err, data) => {
+    if (err) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    if (!data) {
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    res.status(200).json(data);
+  });
 });
 
 app.get("/categories/:id", (req, res) => {
   const payload = req.query;
   const queryCategoriesSelect = `SELECT * FROM categories_post WHERE post_id = "${req.params.id}"`;
   db.query(queryCategoriesSelect, (err, data) => {
-    const { categories_id: category_data } = data;
     if (err) {
       res.status(500).json({ error: "Internal server error" });
       return;
@@ -162,7 +615,7 @@ app.post("/user-login", (req, res) => {
         if (passwordMatch) {
           const token = jwt.sign(
             { id: user.id, username: user.username },
-            "jwtSecret",
+            process.env.JWT_SECRET_KEY,
             {
               expiresIn: 1800,
             }
